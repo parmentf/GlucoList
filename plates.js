431cc5acb31b59559ae53b1d872f2dc02f6d4794
François Parmentier <francois.parmentier@gmail.com>
2012-01-23 21:16:33 +0000

    Added new files

---
+++ plates.js
@@ -0,0 +1,60 @@
+var plates = [
+{
+    id : 1,
+    name : 'Frites',
+    description : 'Un plat de frites',
+    weight : 100,
+    glucids : 40
+},
+{
+    id : 2,
+    name : 'Pommes de terres',
+    description : 'Une assiette de pommes de terres à la vapeur',
+    weight : 100,
+    glucids : 20
+},
+{
+    id : 3,
+    name : 'Spaghettis',
+    description : 'Une assiette de spaghettis nature',
+    weight : 100,
+    glucids : 20
+},
+{
+    id : 4,
+    name : 'Coquillettes',
+    description : 'Une assiette de coquillettes nature',
+    weight : 100,
+    glucids : 20
+},
+{
+    id : 5,
+    name : 'Lentilles',
+    description : 'Une assiette de lentilles',
+    weight : 100,
+    glucids : 20
+},
+{
+    id : 6,
+    name : 'Purée',
+    description : 'Une assiette de purée',
+    weight : 130,
+    glucids : 20
+},
+{
+    id : 7,
+    name : 'Riz',
+    description : 'Une assiette de riz nature',
+    weight : 100,
+    glucids : 20
+},
+{
+    id : 8,
+    name : 'Semoule',
+    description : 'Une assiette de semoule nature',
+    weight : 100,
+    glucids : 20
+}
+];
+
+module.exports.all = plates;
